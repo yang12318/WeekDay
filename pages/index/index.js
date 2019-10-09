1e35641d590997805edd84bd30a5678d9568f4d9
yang12318 <32741884+yang12318@users.noreply.github.com>
2019-10-09 23:29:25 +0800

    10/9 邮箱部分界面

--- pages/index/index.js
+++ pages/index/index.js
@@ -60,6 +60,35 @@ Page({
     TabCur: 0,
     scrollLeft: 0,
   },
+  // ListTouch触摸开始
+  ListTouchStart(e) {
+    this.setData({
+      ListTouchStart: e.touches[0].pageX
+    })
+  },
+
+  // ListTouch计算方向
+  ListTouchMove(e) {
+    this.setData({
+      ListTouchDirection: e.touches[0].pageX - this.data.ListTouchStart > 0 ? 'right' : 'left'
+    })
+  },
+
+  // ListTouch计算滚动
+  ListTouchEnd(e) {
+    if (this.data.ListTouchDirection == 'left') {
+      this.setData({
+        modalName: e.currentTarget.dataset.target
+      })
+    } else {
+      this.setData({
+        modalName: null
+      })
+    }
+    this.setData({
+      ListTouchDirection: null
+    })
+  },
   tabSelect(e) {
     console.log(e.currentTarget.dataset.id)
     this.setData({
